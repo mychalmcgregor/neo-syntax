7b133111379f8cf956933b48be0e974dbe27eba8
fademyke <fademyke@gmail.com>
2017-09-06 13:55:15 -0400

    readme and sample file update

---
+++ sample-files/Elm.elm
@@ -0,0 +1,34 @@
+-- UPDATE
+
+
+module Main exposing (..)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- MAIN
+
+
+main : Program Never Model Msg
+main =
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
